88809873e79ebe6b6b9367863859ff0da9fe1bbe
CheesyChocolate <dev@behnamlal.xyz>
2024-02-18 21:57:36 +0000

    small changes(general plugins): add todo for plugins to replace

--- lua/main/lazy/init.lua
+++ lua/main/lazy/init.lua
@@ -3,6 +3,6 @@ return {
 	"jreybert/vimagit",
 	"airblade/vim-gitgutter", -- TODO: replace with "lewis6991/gitsigns.nvim"
 	"tpope/vim-surround",
-	"tpope/vim-commentary",
+	"tpope/vim-commentary", -- TODO: replace with "numToStr/Comment.nvim"
 	"ap/vim-css-color",
 }
