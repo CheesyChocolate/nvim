e6f4c733cb899ef1f00a6bc56cca69298bdc9bb1
CheesyChocolate <dev@behnamlal.xyz>
2024-02-18 19:12:47 +0000

    note(init): add todo

--- lua/main/lazy/init.lua
+++ lua/main/lazy/init.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-lua/plenary.nvim",
 	"jreybert/vimagit",
-	"airblade/vim-gitgutter",
+	"airblade/vim-gitgutter", -- TODO: replace with "lewis6991/gitsigns.nvim"
 	"tpope/vim-surround",
 	"tpope/vim-commentary",
 	"ap/vim-css-color",
