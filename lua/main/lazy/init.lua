def55e1aae785d471a2991489d42288416dd7f2a
CheesyChocolate <dev@behnamlal.xyz>
2024-02-23 22:09:31 +0000

    change(git plugin): replace vim-gitgutter with gitsigns

--- lua/main/lazy/init.lua
+++ lua/main/lazy/init.lua
@@ -1,8 +1,13 @@
 return {
 	"nvim-lua/plenary.nvim",
 	"jreybert/vimagit",
-	"airblade/vim-gitgutter", -- TODO: replace with "lewis6991/gitsigns.nvim"
 	"tpope/vim-surround",
 	"tpope/vim-commentary", -- TODO: replace with "numToStr/Comment.nvim"
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end
+	},
 	"ap/vim-css-color",
 }
