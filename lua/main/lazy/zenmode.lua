d1c9ec66b6fca2a8ba5e72f5522f5eb380912f0f
CheesyChocolate <dev@behnamlal.xyz>
2024-03-04 05:44:59 +0000

    fix?(zenmode): temporary fix for zenmode in neomutt

--- lua/main/lazy/zenmode.lua
+++ lua/main/lazy/zenmode.lua
@@ -1,10 +1,41 @@
--- TODO: set ZenMode options when opening neomutt temp files
--- vim.api.nvim_create_autocmd(
--- 	{"BufRead", "BufNewFile"}, {
--- 		pattern = {"/tmp/neomutt*"},
--- 		callback =  require("zen-mode").toggle()
--- 	}
--- )
+vim.api.nvim_create_autocmd(
+	{"BufRead", "BufNewFile"}, {
+		pattern = {"/tmp/neomutt*"},
+		callback = function()
+			-- TODO: does not retain the config defined in plugin
+			-- temporary fix
+			require("zen-mode").setup {
+				window = {
+					width = 80,
+					options = {
+						number = false,
+						relativenumber = false,
+						foldcolumn = "0",
+					},
+				},
+				plugins = {
+					options = {
+						enabled = true,
+						linebreak = true,
+					},
+				},
+				on_open = function(win)
+					vim.keymap.set("n", "ZZ", ":ZenMode|x!<CR>", { noremap = true, silent = true })
+					vim.keymap.set("n", "ZQ", ":ZenMode|q!<CR>", { noremap = true, silent = true })
+				end,
+				on_close = function()
+					vim.keymap.del("n", "ZZ")
+					vim.keymap.del("n", "ZQ")
+				end,
+			}
+
+			require("zen-mode").toggle()
+			-- TODO: doesn't work
+			-- switch to second window
+			-- vim.cmd("wincmd w")
+		end
+	}
+)
 
 return {
 	"folke/zen-mode.nvim",
@@ -25,6 +56,7 @@ return {
 						linebreak = true,
 					},
 				},
+				-- TODO: alias :q to :ZenMode | q
 				on_open = function(win)
 					vim.keymap.set("n", "ZZ", ":ZenMode|x!<CR>", { noremap = true, silent = true })
 					vim.keymap.set("n", "ZQ", ":ZenMode|q!<CR>", { noremap = true, silent = true })
