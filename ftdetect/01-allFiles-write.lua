a71a74223be4a3f43fdefa4db4c148e9c1f60823
CheesyChocolate <dev@behnamlal.xyz>
2024-03-12 13:19:42 +0000

    change(ftdetect allFiles): merge autocommands to single one

--- ftdetect/01-allFiles-write.lua
+++ ftdetect/01-allFiles-write.lua
@@ -1,30 +1,18 @@
 -- Automatically deletes all trailing whitespace and newlines at end of file on save. & reset cursor position
+
 vim.api.nvim_create_autocmd(
 	"BufWritePre", {
 		pattern = "*",
-		command = "let currPos = getpos('.')"
+		callback = function()
+			local currPos = vim.fn.getpos(".")
+
+			vim.cmd([[%s/\s\+$//e]])
+			vim.cmd([[%s/\n\+\%$//e]])
+
+			vim.fn.cursor(currPos[2], currPos[3])
+		end
 	}
 )
 
--- delete trailing whitespace
-vim.api.nvim_create_autocmd(
-	"BufWritePre", {
-		pattern = "*",
-		command = [[%s/\s\+$//e]]
-	}
-)
-
--- delete trailing newlines
-vim.api.nvim_create_autocmd(
-	"BufWritePre", {
-		pattern = "*",
-		command = [[%s/\n\+\%$//e]]
-	}
-)
-
-vim.api.nvim_create_autocmd(
-	"BufWritePost", {
-		pattern = "*",
-		command = "cal cursor(currPos[1], currPos[2])"
-	}
-)
+-- TODO: https://www.reddit.com/r/neovim/comments/uvemjr/is_there_an_autocmd_pattern_to_exclude_a_specific/
+-- for ipython files, don't save cursor position
