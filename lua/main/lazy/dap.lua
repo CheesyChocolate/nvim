31527b2a98ea48f3f9f32467ff030fd3c2406daf
CheesyChocolate <dev@behnamlal.xyz>
2024-05-26 13:10:56 +0000

    add(dap): more debuggers

--- lua/main/lazy/dap.lua
+++ lua/main/lazy/dap.lua
@@ -24,7 +24,9 @@ return {
 			-- online, please don't ask me how to install them :)
 			ensure_installed = {
 				-- Update this to ensure that you have the debuggers for the langs you want
+				'debugpy',
 				'delve',
+				'java-debug-adapter',
 			},
 		}
 
