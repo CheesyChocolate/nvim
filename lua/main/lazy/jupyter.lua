1eaac79bb9ef61d5a5150c7d5b6287d9cbe1d118
CheesyChocolate <dev@behnamlal.xyz>
2024-09-04 16:02:32 +0000

    fix(jupyter): open files that lack metadata

--- lua/main/lazy/jupyter.lua
+++ lua/main/lazy/jupyter.lua
@@ -1,6 +1,16 @@
 return {
 	"GCBallesteros/jupytext.nvim",
 	config = function()
-		require("jupytext").setup{}
+		require("jupytext").setup({
+			-- TODO: fix open files that lack metadata
+			output_extension = "py",
+			custom_language_formatting = {
+				ipynb = {
+					extension = "py",
+					style = "hydrogen",
+					force_ft = "python"
+				}
+			}
+		})
 	end
 }
