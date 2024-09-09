e7a6558a601bb0259f1e19e28f472d082befe476
CheesyChocolate <dev@behnamlal.xyz>
2024-09-09 23:44:31 +0000

    small change(neotest): remove unnecessary level of nesting

--- lua/main/lazy/neotest.lua
+++ lua/main/lazy/neotest.lua
@@ -1,36 +1,34 @@
 return {
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-neotest/neotest-go",
-			"nvim-neotest/neotest-python",
-			"rcasia/neotest-java",
-			"alfaix/neotest-gtest",
-			"rcasia/neotest-bash",
-		},
-		config = function()
-			local neotest = require("neotest")
-			neotest.setup({
-				adapters = {
-					require("neotest-go"),
-					require("neotest-python"),
-					require("neotest-java"),
-					require("neotest-gtest").setup({}),
-					require("neotest-bash"),
-				}
-			})
-
-			vim.keymap.set("n", "<leader>tc", function()
-				neotest.run.run()
-			end)
-
-			vim.keymap.set("n", "<leader>tf", function()
-				neotest.run.run(vim.fn.expand("%"))
-			end)
-		end,
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-neotest/neotest-go",
+		"nvim-neotest/neotest-python",
+		"rcasia/neotest-java",
+		"alfaix/neotest-gtest",
+		"rcasia/neotest-bash",
 	},
+	config = function()
+		local neotest = require("neotest")
+		neotest.setup({
+			adapters = {
+				require("neotest-go"),
+				require("neotest-python"),
+				require("neotest-java"),
+				require("neotest-gtest").setup({}),
+				require("neotest-bash"),
+			}
+		})
+
+		vim.keymap.set("n", "<leader>tc", function()
+			neotest.run.run()
+		end)
+
+		vim.keymap.set("n", "<leader>tf", function()
+			neotest.run.run(vim.fn.expand("%"))
+		end)
+	end,
 }
