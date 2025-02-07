a7340725e69463ef28fb05f0f8f0ee27f84f511a
CheesyChocolate <dev@behnamlal.xyz>
2025-02-07 01:25:20 +0000

    fix(language-C): use lua callback instead of vim substitution
    It was wrong anyway, the correct expression is `[[%s/\%$/\r/e]]` however
    lua callback is more efficient.

--- ftdetect/43-language-C.lua
+++ ftdetect/43-language-C.lua
@@ -1,7 +1,9 @@
 -- add trailing newline for ANSI C standard
 vim.api.nvim_create_autocmd(
 	"BufWritePre", {
-		pattern = "*.[ch]",
-		command = [["%s/\%$/\r/e]]
+		pattern = { "*.c", "*.h" },
+		callback = function()
+			vim.api.nvim_buf_set_lines(0, -1, -1, false, { "" })
+		end
 	}
 )
