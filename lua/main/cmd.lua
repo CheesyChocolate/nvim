a71ad03b9353bffa48bd409d2adeca71116632ca
CheesyChocolate <dev@behnamlal.xyz>
2025-06-17 10:30:12 +0000

    small change(cmd): simplify Toggle_hidden_all function
    - i don't know, it looks simpler to me. my friend disagrees.

--- lua/main/cmd.lua
+++ lua/main/cmd.lua
@@ -11,24 +11,12 @@ vim.api.nvim_create_user_command(
 )
 
 -- Function for toggling the bottom statusbar:
-local h = {hidden_all = 0}
-
+local hidden_all = false
 function Toggle_hidden_all()
-	local hideOptions = {}
-	if h.hidden_all == 0 then
-		h.hidden_all = 1
-		vim.opt.showmode = false
-		vim.opt.ruler = false
-		vim.opt.laststatus = 0
-		vim.opt.showcmd = false
-		vim.opt.cmdheight = 0
-
-	else
-		h.hidden_all = 0
-		vim.opt.showmode = true
-		vim.opt.ruler = true
-		vim.opt.laststatus = 2
-		vim.opt.showcmd = true
-		vim.opt.cmdheight = 1
-	end
+	vim.opt.showmode = hidden_all
+	vim.opt.ruler = hidden_all
+	vim.opt.laststatus = hidden_all and 2 or 0
+	vim.opt.showcmd = hidden_all
+	vim.opt.cmdheight = hidden_all and 1 or 0
+	hidden_all = not hidden_all
 end
