8ee766c3eb60eee8971ae9ec1d1089010643e89c
CheesyChocolate <dev@behnamlal.xyz>
2026-06-08 07:03:18 +0000

    chore(lsp): lua format

--- lua/main/lazy/lsp.lua
+++ lua/main/lazy/lsp.lua
@@ -105,6 +105,16 @@ return {
 			-- 			capabilities = capabilities,
 			-- 			settings = {
 			-- 				Lua = {
+			--					runtime = {
+			--						version = 'LuaJIT',
+			--					},
+			--					diagnostics = {
+			--						globals = { 'vim' },
+			--					},
+			--					workspace = {
+			--						library = vim.api.nvim_get_runtime_file("", true),
+			--						checkThirdParty = false,
+			--					},
 			-- 					format = {
 			-- 						enable = true,
 			-- 						defaultConfig = {
@@ -131,24 +141,24 @@ return {
 		cmp.setup({
 			snippet = {
 				expand = function(args)
-					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-				['<C-y>'] = cmp.mapping.confirm({ select = true }),
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				["<C-Space>"] = cmp.mapping.complete(),
 			}),
 			sources = cmp.config.sources({
-				{ name = "copilot", group_index = 2 },	-- TODO: should I?
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },
-				{ name = 'nvim-lsp-signature-help' },
-				{ name = 'path' },
+				{ name = "copilot", group_index = 2 }, -- TODO: should I?
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "nvim-lsp-signature-help" },
+				{ name = "path" },
 			}, {
-				{ name = 'buffer' },
-			})
+				{ name = "buffer" },
+			}),
 		})
 
 		vim.diagnostic.config({
@@ -163,10 +173,9 @@ return {
 			},
 		})
 
-		vim.api.nvim_create_autocmd('LspAttach', {
-			group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
-
 				local client = vim.lsp.get_client_by_id(ev.data.client_id)
 				if client and client.server_capabilities.inlayHintProvider then
 					vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
@@ -176,32 +185,52 @@ return {
 					vim.lsp.codelens.refresh()
 					vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
 						buffer = ev.buf,
-						callback = vim.lsp.codelens.refresh
+						callback = vim.lsp.codelens.refresh,
 					})
 				end
 
 				local opts = { buffer = ev.buf }
 				opts.desc = "Go to Definition"
-				vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
 				opts.desc = "Show hover information"
-				vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
 				opts.desc = "View Workspace Symbols"
-				vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+				vim.keymap.set("n", "<leader>vws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
 				opts.desc = "View diagnostics"
-				vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
 				opts.desc = "View Code Actions"
-				vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+				vim.keymap.set("n", "<leader>vca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
 				opts.desc = "View RefeRences"
-				vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+				vim.keymap.set("n", "<leader>vrr", function()
+					vim.lsp.buf.references()
+				end, opts)
 				opts.desc = "ReName symbol"
-				vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+				vim.keymap.set("n", "<leader>vrn", function()
+					vim.lsp.buf.rename()
+				end, opts)
 				opts.desc = "Show signature help"
-				vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
 				opts.desc = "Go to next diagnostic"
-				vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
 				opts.desc = "Go to previous diagnostic"
-				vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
-			end
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+			end,
 		})
-	end
+	end,
 }
